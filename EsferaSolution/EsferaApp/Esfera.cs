bf9e8af6b8dd8c75468d12b630b9bb49060aa87c
Gabriel Moura <gabo3900@outlook.com>
2019-08-07 14:02:00 -0300

    Init

---
+++ EsferaSolution/EsferaApp/Esfera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsferaApp
+{
+    class Esfera
+    {
+        private double raio;
+        public void SetRaio(double r) { raio = r; }
+        public double GetRaio() { return raio; }
+        public double CalcArea() { return Math.PI * raio * raio; }
+        public double CalcVolume() { return 4 * Math.PI * raio * raio * raio / 3; }
+    }
+}
