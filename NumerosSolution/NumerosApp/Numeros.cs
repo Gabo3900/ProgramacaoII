6ce8555784e72d0baf9bda32e5280850ce64300e
Gabriel Moura <gabo3900@outlook.com>
2019-08-13 10:24:30 -0300

    Init

---
+++ NumerosSolution/NumerosApp/Numeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosApp
+{
+    class Numeros
+    {
+        private int inicio;
+        private int fim;
+        public Numeros(int i, int f)
+        {
+            inicio = i;
+            fim = f;
+        }
+        public int[] Calcular(bool p, bool i)
+        {
+            
+        }
+    }
+}
