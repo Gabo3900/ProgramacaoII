7cdbc8e4e335c0e81ac189dc91e397a6a564830c
Gabriel Moura <gabo3900@outlook.com>
2019-08-16 11:34:20 -0300

    Init

---
+++ EquacaoIIGrauSolution/EquacaoIIGrauApp/EquacaoIIGrau.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquacaoIIGrauApp
+{
+    class EquacaoIIGrau
+    {
+        private double a, b, c;
+        public void SetABC(double a, double b, double c)
+        {
+            this.a = a; this.b = b; this.c = c;
+        }
+
+        public void GetABC(out double a, out double b, out double c)
+        {
+            a = this.a; b = this.b; c = this.c;
+        }
+        public bool RaizesReais()
+        {
+            return Delta() >= 0;
+        }
+        public double Delta()
+        {
+            return b * b - 4 * a * c;
+        }
+        public bool X1(out double x)
+        {
+            x = 0;
+            if (RaizesReais() == false || a == 0) return false;
+            x = (-b + Math.Sqrt(Delta())) / (2 * a);
+            return true;
+        }
+        public bool X2(out double x)
+        {
+            x = 0;
+            if (RaizesReais() == false || a == 0) return false;
+            x = (-b - Math.Sqrt(Delta())) / (2 * a);
+            return true;
+        }
+    }
+}
