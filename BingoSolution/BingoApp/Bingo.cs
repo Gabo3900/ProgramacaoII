7b341bf48e1225606440ffe5a548bc57e2ebc42f
GABRIEL DOS SANTOS MOURA <g.moura@academico.ifrn.edu.br>
2019-08-21 13:46:05 -0300

    Init

---
+++ BingoSolution/BingoApp/Bingo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingoApp
+{
+    class Bingo
+    {
+        private int numBolas;
+        public void Iniciar(int numBolas) { this.numBolas = numBolas; }
+        public int Proximo() {
+            int r;
+            Random random = new Random();
+            for (int i = 0; i < numBolas; i++) {
+                r = random.Next(numBolas);
+                foreach (int a in Sorteados())
+                if (r != a)
+                    return r;
+            }
+            return -1;
+        }
+        public int[] Sorteados() { }
+    }
+}
