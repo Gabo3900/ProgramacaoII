2079ba2e91eefee1dd5efde68d6118075cde7a9a
GABRIEL DOS SANTOS MOURA <g.moura@academico.ifrn.edu.br>
2019-08-27 08:49:19 -0300

    Init

---
+++ HistoricoSolution/HistoricoApp/Disciplina.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricoApp
+{
+    class Disciplina
+    {
+        private string nome;
+        private string semestre;
+        private int media;
+        private bool aprovado;
+        public int GetMedia() { return media; }
+    }
+}
---
+++ HistoricoSolution/HistoricoApp/Historico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoricoApp
+{
+    class Historico
+    {
+        private string aluno;
+        private Disciplina[] discs = new Disciplina[10];
+        private int k;
+        public void Inserir(Disciplina d) { discs[k++] = d; }
+        public Disciplina[] Listar()
+        {
+            Disciplina[] d = new Disciplina[k];
+            Array.Copy(discs, d, k);
+            return d;
+        }
+        public double CalcularIRA()
+        {
+            int soma = 0;
+            foreach (Disciplina d in Listar()) { soma += d.GetMedia(); }
+            return soma / k;
+        }
+    }
+}
