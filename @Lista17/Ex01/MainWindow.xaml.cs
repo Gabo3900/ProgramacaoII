bb060ed1359164956f62a1589ff38f269398b843
Gabriel Moura <gabo3900@outlook.com>
2019-09-13 11:40:51 -0300

    Init

--- @Lista17/Ex01/MainWindow.xaml.cs
+++ @Lista17/Ex01/MainWindow.xaml.cs
@@ -24,5 +24,28 @@ namespace Ex01
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DisciplinaSemestral x = new DisciplinaSemestral(
+                snome.Text, int.Parse(s1bim.Text), int.Parse(s2bim.Text),
+                int.Parse(sfinal.Text));
+            list.Items.Add(x);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DisciplinaAnual x = new DisciplinaAnual(
+                anome.Text, int.Parse(a1bim.Text), int.Parse(a2bim.Text),
+                int.Parse(a3bim.Text), int.Parse(a4bim.Text),
+                int.Parse(afinal.Text));
+            list.Items.Add(x);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            IMedia x = list.SelectedItem as IMedia;
+            MessageBox.Show(x.GetMedia().ToString());
+        }
     }
 }
