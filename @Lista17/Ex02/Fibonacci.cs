cfadb579520370fcb44e8aef85e271a6342f1b8c
Gabriel Moura <gabo3900@outlook.com>
2019-09-17 10:15:01 -0300

    Init

---
+++ @Lista17/Ex02/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    class Fibonacci : ISequencia
+    {
+        private int a;
+        private int b;
+        public void Iniciar() { a = 0; b = 1; }
+        public int Proximo() {
+            int aux = a;
+            a = b;
+            b = a + aux;
+            return b;
+        }
+    }
+}
