7a99909c4994824d11f2b0ce5824b5acee1456d0
Gabriel Moura <gabo3900@outlook.com>
2019-09-17 10:27:53 -0300

    Init

---
+++ @Lista17/Ex02/PA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    class PA
+    {
+        private int primeiroElemento;
+        private int razao;
+        public void Iniciar()
+        {
+            primeiroElemento = 0;
+        }
+        public int Proximo()
+        {
+            primeiroElemento += razao;
+            return primeiroElemento;
+        }
+    }
+}
