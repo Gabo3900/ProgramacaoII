7d6de3bc1d820068d2777e4c1cea0b8c3744ea35
Gabriel Moura <gabo3900@outlook.com>
2019-09-24 12:52:43 -0300

    Init

---
+++ @Lista17/Ex04/Pais.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Pais : IComparable
+    {
+        private string nome;
+        private int ouro;
+        private int prata;
+        private int bronze;
+        public Pais(string n, int o, int p, int b)
+        {
+            nome = n;
+            ouro = o;
+            prata = p;
+            bronze = b;
+        }
+        public int CompareTo(object outro)
+        {
+            Pais w = outro as Pais;
+            if (ouro < w.ouro) return 1;
+            if (ouro > w.ouro) return -1;
+
+            if (prata < w.prata) return 1;
+            if (prata > w.prata) return -1;
+
+            if (bronze < w.bronze) return 1;
+            if (bronze > w.bronze) return -1;
+
+            return nome.CompareTo(w.nome);
+        }
+        public override string ToString()
+        {
+            return nome;
+        }
+    }
+}
---
+++ @Lista17/Ex04/QuadroMedalhas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class QuadroMedalhas
+    {
+        private Pais[] paises = new Pais[20];
+        private int k;
+        public void Inserir(Pais p)
+        {
+            paises[k++] = p;
+        }
+        public Pais[] Listar()
+        {
+            Pais[] t = new Pais[k];
+            Array.Copy(paises, t, k);
+            Array.Sort(t);
+            return t;
+        }
+    }
+}
