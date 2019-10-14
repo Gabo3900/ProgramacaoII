890a2618e75d5ecd2d2ffd709eedf1f726e51d27
Gabriel Moura <gabo3900@outlook.com>
2019-10-14 15:42:33 -0300

    init

---
+++ @Lista20/Ex07/Veiculo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    class Veiculo
+    {
+        private string placa;
+        private string fabricante;
+        private string modelo;
+        private int ano;
+        private decimal preco;
+        private bool vendido;
+        public Veiculo(string p, string f, string m, int a, decimal pr)
+        {
+            placa = p;
+            fabricante = f;
+            modelo = m;
+            ano = a;
+            preco = pr;
+        }
+        public void Vender()
+        {
+            vendido = true;
+        }
+        public string GetPlaca()
+        {
+            return fabricante;
+        }
+        public string GetFabricante()
+        {
+            return fabricante;
+        }
+        public string GetModelo()
+        {
+            return modelo;
+        }
+        public decimal GetPreco()
+        {
+            return preco;
+        }
+        public bool Vendido()
+        {
+            return vendido;
+        }
+    }
+}
